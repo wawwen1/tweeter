427df6a50531b6bc66effb3fd2e8f815d0b4bf12
warren roque <warrenroque17@gmail.com>
2022-03-30 16:13:20 +0000

    refactor code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,27 +1,17 @@
 //loads functions after the document is ready
 $(document).ready(function() {
 
-  const textArea = document.getElementById("tweet-text");
-  textArea.addEventListener("keyup", function(event) {
-  const charCount = $(this).val().length;
-  console.log(charCount);
-  const counter = $(this).next(".tweet-submit").find(".counter");
+  $("#tweet-text").keyup(function(event) {
+    const charCount = $(this).val().length;
+    const counter = $(this).closest(".tweet-new").find(".counter");
+    //const counter = $(this).next(".tweet-submit").find(".counter");
+    $(counter).text(140 - charCount);
 
-  $(counter).text(140 - charCount);
-
-  if (charCount > 140) {
-    $(counter).css("color", "red");
-  } else {
-    $(counter).css("color", "#545149");
-  }
-
+    if (charCount > 140) {
+      $(counter).css("color", "red");
+    } else {
+      $(counter).css("color", "#545149");
+    }
   });
-
-  // $("#tweet-text").keyup(function(event) {
-  //   const charCount = $(this).val().length;
-  //   console.log(charCount);
-  //   $(this).closest(".counter").text(140 - charCount);
-
-  //});
 });
 
