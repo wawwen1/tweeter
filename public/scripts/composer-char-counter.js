51fc088fc36847a1f3fb00fe109bab2d094a4e2e
warren roque <warrenroque17@gmail.com>
2022-03-30 19:22:45 +0000

    remove unused code

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,15 +2,15 @@
 $(document).ready(function() {
 
   $("#tweet-text").keyup(function(event) {
-    const $charCount = $(this).val().length;
-    const $counter = $(this).closest(".tweet-new").find(".counter");
+    const charCount = $(this).val().length;
+    const counter = $(this).closest(".tweet-new").find(".counter");
     //const counter = $(this).next(".tweet-submit").find(".counter");
-    $($counter).text(140 - $charCount);
+    $(counter).text(140 - charCount);
 
-    if ($charCount > 140) {
-      $($counter).css("color", "red");
+    if (charCount > 140) {
+      $(counter).css("color", "red");
     } else {
-      $($counter).css("color", "#545149");
+      $(counter).css("color", "#545149");
     }
   });
 });
