117ee26f331f7666af3b46c5a69aa86b853de9f0
warren roque <warrenroque17@gmail.com>
2022-03-30 15:25:27 +0000

    configure counter

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,12 +2,21 @@
 $(document).ready(function() {
 
   const textArea = document.getElementById("tweet-text");
-  textArea.addEventListener("keypress", (event) => {
-    console.log(event);
+  textArea.addEventListener("keyup", function(event) {
+  const char = $(this).val().length;
+  console.log(char);
+
+  const counter = $(".counter").text(140 - char);
+  console.log(`counter: ${counter}`);
+ 
   })
 
-  // $("tweet-text").on("keypress", function(event) {
-  //   console.log(event.val().length);
+  // $("#tweet-text").on("keypress", function(event) {
+  //   const charCount = $(this).val().length;
+  //   console.log(charCount);
+  //   const counter = $(this).find("#tweet-text .counter")
+  //   console.log(counter.val);
+  // //  console.log('counter: ' + counter.val(140 - charCount));
   // });
 
 
