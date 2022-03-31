2d67d29e94283c39533205b52549cf4836722e0a
warren roque <warrenroque17@gmail.com>
2022-03-31 06:05:19 +0000

    add document.ready function + edit variable names

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,48 +4,78 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetData = {
-
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
-    "handle": "@SirIsaac"
+const tweetData = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1648532134625
   },
-  "content": {
-    "text": "If I have seen further it is by standing on the shoulders of giants"
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1648618534625
   },
-  "created_at": 1648494535685
-
-}
-
-const renderTweets = (tweets) {
-  for (let tweet of tweets) {
-    $("#tweet-posts").prepend(createTweetElement(tweet));
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Ocean father, I was wrong. Years I follow just the sun. Now I see your darkness holds the key. I close my eyes and I begin to see"
+    },
+    "created_at": 1648618534625
   }
-}
+];
 
-const createTweetElement = (tweet) => {
-  let $tweet = `
-  <article class="tweet">
+$(document).ready(function() {
+
+  
+  const renderTweets = function(tweets) {
+    for (let tweet of tweets) {
+      $("#tweet-posts").append(createTweetElement(tweet));
+    }
+  }
+  
+  const createTweetElement = function(tweet) {
+    let markup = `
+    <article class="tweet">
     <header class="tweet-user">
-      <img class="tweet-avatar" src="${tweet.user.avatars}">
-        <h4 class="tweet-username">${tweet.user.name}</h4>
-        <h4 class="tweet-handle">${tweet.user.handle}</h4>
+    <img class="tweet-avatar" src="${tweet.user.avatars}">
+    <h4 class="tweet-username">${tweet.user.name}</h4>
+    <h4 class="tweet-handle">${tweet.user.handle}</h4>
     </header>
-
+    
     <div class="tweet-content">
-      <p>${tweet.content.text}</p>
+    <p>${tweet.content.text}</p>
     </div>
-
+    
     <footer class="tweet-extras">
-      <span class="tweet-date">${tweet.created_at}</span>
-      <span class="tweet-icons">
-        <i class="fa-solid fa-flag"></i>
-        <i class="fa-solid fa-retweet"></i>
-        <i class="fa-solid fa-heart"></i>
-      </span>
+    <span class="tweet-date">${tweet.created_at}</span>
+    <span class="tweet-icons">
+    <i class="fa-solid fa-flag"></i>
+    <i class="fa-solid fa-retweet"></i>
+    <i class="fa-solid fa-heart"></i>
+    </span>
     </footer>
-  </article>      
-  `;
-  return $tweet;
-};
+    </article>      
+    `;
+    
+    return markup;
+  };
+  
+  renderTweets(tweetData);
+  
+});
